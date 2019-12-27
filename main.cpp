56dfedd87da59d2c876c179bcc7e18c66de9e447
fummicc1 <fumiya.tennis1@gmail.com>
2019-12-28 03:17:50 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -22,14 +22,17 @@ int main(int argc, char const *argv[])
     std::cout << gray.size() << std::endl;
     std::cout << "cvtColor" << std::endl;
     gray.copySize(gray2);
-    cv::medianBlur(gray, gray2, 5);
+    cv::medianBlur(gray, gray2, 3);
     std::cout << "medianBlur" << std::endl;
-    cv::adaptiveThreshold(gray, edges, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 35, 9);
+    cv::adaptiveThreshold(gray, edges, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 9, 9);
     std::cout << "adaptiveThreshold" << std::endl;
     cv::bilateralFilter(source, color, 9, 300, 300);
     std::cout << "bilateralFilter" << std::endl;
     cv::bitwise_and(color, color, distribution, edges);
     std::cout << "bitwise_and" << std::endl;
+
+    cv::xphoto::oilPainting(distribution, distribution, 5, 1, cv::COLOR_LRGB2Lab);
+    std::cout << "oilPainting" << std::endl;
 
     cv::namedWindow("window", cv::WINDOW_AUTOSIZE);
     cv::imshow("window", distribution);
